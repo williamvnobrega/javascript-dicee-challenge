cd677113d15793fca3d16f2d3d1af5d235621a19
williamvnobrega <williamvnobrega@gmail.com>
2023-11-06 16:21:06 -0300

    project created

---
+++ index.js
@@ -0,0 +1,19 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+let randomImageSource = "images/" + randomDiceImage;
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+//randomize 2nd dice
+let randomNumber2 = Math.floor(Math.random()*6) + 1;
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins !";
+}else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins !";
+}else{
+    document.querySelector("h1").innerHTML = "Draw !";
+}
+
